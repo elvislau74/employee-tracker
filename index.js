f57d58bba4f2ecb7354f92a458b93909a85388c3
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-06-07 19:00:26 -0400

    add all dependencies on top of index.js, add code to create connection to mysql through promises

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const inquirer = require('inquirer');
 const {table} = require('table');
+const mysql = require('mysql/promise');
 
+let db;
+mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'yoshiharu',
+        database: 'human_resources_db'
+    }
+)
+.then((connection) => {
+    db = connection;
+    console.log('Connected to the human_resources_db database.');
+})
