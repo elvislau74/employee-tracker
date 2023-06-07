ae06f01ad44f52e271f9351db22d5ab23ebd08ca
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-06-07 19:07:58 -0400

    fix bug in mysql dependency in index.js, add an async function to show table

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const {table} = require('table');
-const mysql = require('mysql/promise');
+const mysql = require('mysql2/promise');
 
 let db;
 mysql.createConnection(
@@ -15,3 +15,40 @@ mysql.createConnection(
     db = connection;
     console.log('Connected to the human_resources_db database.');
 })
+
+const config = {
+    border: {
+      topBody: `─`,
+      topJoin: `┬`,
+      topLeft: `┌`,
+      topRight: `┐`,
+  
+      bottomBody: `─`,
+      bottomJoin: `┴`,
+      bottomLeft: `└`,
+      bottomRight: `┘`,
+  
+      bodyLeft: `│`,
+      bodyRight: `│`,
+      bodyJoin: `│`,
+  
+      joinBody: `─`,
+      joinLeft: `├`,
+      joinRight: `┤`,
+      joinJoin: `┼`
+    }
+  };
+
+  async function showTable (data){
+    let tableData = [];
+    // option 1 fancy one line table data format
+    tableData = [Object.keys(data[0]), ...data.map(val => Object.values(val))];
+  
+    const answers = await inquirer.prompt([
+      {
+        message: "\n" + table(tableData, config),
+        type: 'input',
+        name: 'name'
+      }
+    ]);
+  }
