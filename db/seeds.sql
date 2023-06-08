db903376c7ec28d1bf9a6cc152f8d138165d3d6c
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-06-07 21:21:28 -0400

    add code into seeds.sql to populate the rows in all the tables

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (name)
+VALUES ("Sales"),
+       ("Engineering"),
+       ("Finance"),
+       ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1),
+       ("Salesperson", 80000, 1),
+       ("Lead Engineer", 150000, 2),
+       ("Software Engineer", 120000, 2),
+       ("Account Manager", 160000, 3),
+       ("Accountant", 125000, 3),
+       ("Legal Team Lead", 250000, 4),
+       ("Lawyer", 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Yoichi", "Isagi", 1, null),
+       ("Meguru", "Bachira", 2, 1),
+       ("Reo", "Mikage", 3, null),
+       ("Seishiro", "Nagi", 4, 3),
+       ("Yo", "Hiori", 5, null),
+       ("Ranze", "Kurona", 6, 5),
+       ("Ikki", "Niko", 7, null),
+       ("Alexis", "Ness", 8, 7);
